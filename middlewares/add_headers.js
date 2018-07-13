9fba68ffa6abacc253bcae76a9d24ed9c6bfd2d0
Maria Del Carmen <mdcra1997@outlook.com>
2018-07-13 14:45:58 -0600

    se añadio cabecera para acceso con cualquier proyecto

--- middlewares/add_headers.js
+++ middlewares/add_headers.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next)=>{
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://proyecto-198807.firebaseapp.com');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     // Request headers you wish to allow
