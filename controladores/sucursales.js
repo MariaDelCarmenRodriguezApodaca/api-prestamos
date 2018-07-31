75a37df777b18943aa3a60d07ccb77b966ff052c
Maria Del Carmen <mdcra1997@outlook.com>
2018-07-30 21:24:51 -0600

    se quito el status de la sucursal

--- controladores/sucursales.js
+++ controladores/sucursales.js
@@ -28,7 +28,7 @@ let getSucursal = (req,res)=>{
 let nuevaSucursal= (req,res)=>{
     var data = req.body;
     if(!data.nombre ||  !data.callenum || !data.colonia || !data.poblacion || !data.municipio || !data.estado  || !data.hora_inicio || !data.hora_fin || !data.telefono || !data.encargado  ) return res.status(403).send({message:`Faltaron datos en la peticion`}); 
-    var sql=`INSERT INTO sucursales VALUES (null, '${data.nombre}','${data.callenum}','${data.colonia}','${data.poblacion}','${data.municipio}','${data.estado}','${data.status}','${data.hora_inicio}','${data.hora_fin}','${data.encargado}','${data.telefono}')`;
+    var sql=`INSERT INTO sucursales VALUES (null, '${data.nombre}','${data.callenum}','${data.colonia}','${data.poblacion}','${data.municipio}','${data.estado}','${data.hora_inicio}','${data.hora_fin}','${data.encargado}','${data.telefono}')`;
     var connection = dbConnection();
     connection.query(sql,(err,result)=>{
         if(err)  res.status(500).send({message:`Error al hacer la consulta a la base de datos ${err} SQL= ${sql}`});
@@ -42,7 +42,7 @@ let nuevaSucursal= (req,res)=>{
 let updateSucursal = (req, res )=>{
     var data = req.body;
     if(!data.nombre ||  !data.callenum || !data.colonia || !data.poblacion || !data.municipio || !data.estado ||  !data.hora_inicio || !data.hora_fin || !data.telefono  ) return res.status(403).send({message:`Faltaron datos en la peticion`}); 
-    var sql=`UPDATE sucursales set nombre='${data.nombre}', callenum = '${data.callenum}', colonia='${data.colonia}', poblacion='${data.poblacion}', municipio='${data.municipio}',estado='${data.estado}', status='${data.status}',hora_inicio ='${data.hora_inicio}',hora_fin='${data.hora_fin}',telefono='${data.telefono}',encargado='${data.encargado}' WHERE idsucursal=${req.params.id}`;
+    var sql=`UPDATE sucursales set nombre='${data.nombre}', callenum = '${data.callenum}', colonia='${data.colonia}', poblacion='${data.poblacion}', municipio='${data.municipio}',estado='${data.estado}',hora_inicio ='${data.hora_inicio}',hora_fin='${data.hora_fin}',telefono='${data.telefono}',encargado='${data.encargado}' WHERE idsucursal=${req.params.id}`;
     var connection = dbConnection();
     connection.query(sql,(err,result)=>{
         if(err) res.status(500).send({message:`ERROR ${err}`});
